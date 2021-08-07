3922585d06081634e4156699de884e86bd3f0d86
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-08-08 00:33:00 +0300

    TekSayilarinToplami

---
+++ TekSayilarinToplami/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("Enter number(To exit enter oddone");
+        int sayi = scanner.nextInt();
+        while(sayi%2==0){
+            numbers.add(sayi);
+            System.out.println("Enter number(To exit enter oddone");
+            sayi=scanner.nextInt();
+        }
+
+        for(int number: numbers){
+            if(number%4==0){
+                System.out.print(number+",");
+            }
+        }
+        System.out.println();
+    }
+}
