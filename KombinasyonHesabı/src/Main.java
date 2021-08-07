d8a0284f1129aa59a45b7f7c2ed0625aba948970
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-08-08 00:48:15 +0300

    Kombinasyon

---
+++ KombinasyonHesabı/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the n: ");
+        int n = scanner.nextInt();
+        System.out.println("Enter the r:");
+        int r = scanner.nextInt();
+        int _n=n;
+        int _r=r;
+        System.out.println("C(n,r) = n!/(r!*(n-r)!): ");
+        for(int i=n;i!=1;i--){
+            _n*=i;
+        }
+        for(int i=r;i!=1;i--){
+            _r*=i;
+        }
+        int _nr=n-r;
+        for(int i=_nr;i!=1;i--){
+            _nr*=i;
+        }
+        System.out.println(_n/(_r*_nr));
+
+
+
+    }
+}
+
