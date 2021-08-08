3e1ad1037220e2725a33e6439a69cc9c8f2c3aea
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-08-08 16:00:11 +0300

    recursive

---
+++ Exercises/src/Recursive.java
@@ -0,0 +1,16 @@
+public class Recursive {
+    public static void main(String[] args) {
+        desen(16,true);
+    }
+    static int desen(int number,boolean yon){
+
+        if(yon){
+            System.out.print(number+" ");
+            return desen(number-5,(number-5)>0);
+        }else{
+            System.out.println(number+" ");
+            return desen(number+5,false);
+        }
+
+    }
+}
