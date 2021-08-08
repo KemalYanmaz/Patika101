7e9bfca1cabb097732071a0712fc320fb87904c6
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-08-09 00:58:01 +0300

    SortingArray

--- Exercises/src/sortingArray.java
+++ Exercises/src/sortingArray.java
@@ -3,29 +3,32 @@ import java.util.Scanner;
 public class sortingArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Dizinin boyutunu girin: ");
-        int boyut = scanner.nextInt();
-        int[] dizi = new int[boyut];
-        System.out.print("Dizinin elemanlarını girin: ");
-        for(int i=0;i<dizi.length;i++){
-            System.out.println("Eleman "+(i+1));
-            dizi[i]= scanner.nextInt();
+
+        int[][] matris = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        for(int i=0;i< matris.length;i++){
+            for(int j=0;j<matris[i].length;j++){
+                System.out.print(matris[i][j]+ " ");
+            }
+            System.out.println();
         }
 
-        for(int i =0;i<dizi.length;i++){
-            for(int j=0;j<dizi.length;j++){
-                int temp;
-                if(dizi[i]<dizi[j] && i>j){
-                    temp = dizi[i];
-                    dizi[i] = dizi[j];
-                    dizi[j] = temp;
-                }
+        int[][] transpose = new int[3][3];
+        for(int i=0;i< transpose.length;i++){
+            for(int j=0;j<transpose[i].length;j++){
+                transpose[j][i]= matris[i][j];
             }
         }
 
-        for(int i:dizi){
-            System.out.print(i+" ");
+        for(int i=0;i< transpose.length;i++){
+            for(int j=0;j< transpose[i].length;j++){
+                System.out.print(transpose[i][j]+ " ");
+            }
+            System.out.println();
         }
-
     }
 }
