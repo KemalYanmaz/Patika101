37ad2bb988e394979c45101a5337c2eaf51c6942
KemalYanmaz <47372602+KemalYanmaz@users.noreply.github.com>
2021-08-08 15:04:46 +0300

    Fibonacciwithrecursive

---
+++ Exercises/src/FibonacciWithRecursive.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FibonacciWithRecursive {
+    public static void main(String[] args) {
+        System.out.print("Kaç tane sayı istiyorsunuz: ");
+        int number = new Scanner(System.in).nextInt();
+        for(int i = 1; i<=number; i++){
+            System.out.print(fibonacci(i)+" ");
+        }
+    }
+    static int fibonacci(int number){
+        if(number==1||number==2){
+            return 1;
+        }
+        return fibonacci(number-1)+fibonacci(number-2);
+    }
+}
